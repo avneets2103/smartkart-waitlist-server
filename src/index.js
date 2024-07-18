14d7b8c031a44a119a11434d95053abdb8b2f840
Avneet Singh <avneetsingh@Avneets-MacBook-Air.local>
2024-07-18 19:12:08 +0530

    ping every 10mins

--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
 import dotenv from 'dotenv';
 import { connectDB } from './DB/index.js';
 import { app } from './app.js';
+import axios from 'axios';
 dotenv.config({
     path: "./env"
 })
+
+const API_URL = "https://smartkart-server.onrender.com/api/v1/ping/ping";
+const makeAPICall = async () => {
+    try {
+        axios.get(API_URL);
+        console.log("Made api call");
+    } catch (error) {
+        console.error("Error making API call:", error);
+    }
+};
 
 connectDB()
 .then(()=>{
     app.listen(process.env.PORT || 8000, ()=>{
         console.log("Server listening on port " + process.env.PORT);
     })
+    // Call makeAPICall every 5 minutes (300000 milliseconds)
+    setInterval(makeAPICall, 1000*60*10);
 })
 .catch((err)=>{
     console.log("MONGO DB connection failed !", err);
